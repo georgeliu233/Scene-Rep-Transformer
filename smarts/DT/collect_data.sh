69a5c3866d449141212a307753564269ecfe49c4
George Liu <46886579+georgeliu233@users.noreply.github.com>
2023-08-23 15:21:49 +0800

    Delete smarts directory

--- smarts/DT/collect_data.sh
+++
@@ -1,5 +0,0 @@
-nohup python3 /home/haochen/TPDM_transformer/DT/decision-transformer/collect_data.py>>collect_data_lt.out --scenario=left_turn 2>&1 &
-nohup python3 /home/haochen/TPDM_transformer/DT/decision-transformer/collect_data.py>>collect_data_cr.out --scenario=cross 2>&1 &
-nohup python3 /home/haochen/TPDM_transformer/DT/decision-transformer/collect_data.py>>collect_data_re.out --scenario=re 2>&1 &
-nohup python3 /home/haochen/TPDM_transformer/DT/decision-transformer/collect_data.py>>collect_data_rm.out --scenario=rm 2>&1 &
-nohup python3 /home/haochen/TPDM_transformer/DT/decision-transformer/collect_data.py>>collect_data_r.out --scenario=r 2>&1 &
